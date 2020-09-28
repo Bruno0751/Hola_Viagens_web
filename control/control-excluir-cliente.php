3a6bef58ce0e84fb64416428319e28ecda25a87c
Bruno Gressler da Silveira <brunogressler1@gmail.com>
2020-09-27 21:31:39 -0300

    mundando usuarios para clientes

---
+++ control/control-excluir-cliente.php
@@ -0,0 +1,14 @@
+<?php
+    ob_start();
+    session_start();
+
+    include '../dao/dao-usuario.php';
+
+    $daoCliente = new DAOCliente();
+    $daoCliente->deletarCliente($_GET['id']);
+
+    $_SESSION['msg'] = "Cliente Excluido";
+
+    header("location:../clientes-cadastrados.php");
+
+    ob_end_flush();
---
+++ control/control-verificacao-de-cliente.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+    ob_start();
+
+    require_once '../model/cliente.php';
+    require_once '../dao/dao-usuario.php';
+    include '../utl/validacao.php';
+                
+    $c = new Cliente();
+
+    $c->login = Validacao::antiXSS($_POST['textLogin']);
+    $c->senha = Validacao::antiXSS($_POST['passSenha']);
+                
+    $clienteDAO = new DAOCliente();
+    $cliente = $clienteDAO->verificarLoginCliente($c);
+
+    if($cliente != null){
+        $_SESSION['msg'] = "Cliente Inválido";
+        header("location:../login-de-usuario.php.");
+
+    }else{
+        $_SESSION['privateUser'] = serialize($cliente);
+
+        header("location:../login-de-cliente.php");
+
+    }
